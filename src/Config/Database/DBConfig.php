9db942933cd3d017131fa1484d6261f44ccd5e69
Krystian <janczakkrystian@gmail.com>
2018-03-17 18:24:36 +0100

    Create project.

---
+++ src/Config/Database/DBConfig.php
@@ -0,0 +1,14 @@
+<?php
+namespace Config\Database;
+
+class DBConfig
+{
+    //nazwa bazy danych
+    public static $databaseName = 'BiOD';
+    //dane dostępowe do bazy danych
+    public static $hostname = 'localhost';
+    public static $databaseType = 'mysql';
+    public static $port = '3306';
+    public static $user = 'root';
+    public static $password = '';
+}
